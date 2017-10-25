fe2f877f1f2e3da8897801675279eccb34619e4e
Odunayo Obajuluwa <odunayoobajuluwa@Odunayos-MacBook-Pro.local>
2017-10-25 16:34:23 -0500

    basic working weather page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-console.log("We're running js!!!")
+console.log("We're running js!!!");
 
 var Geo={};
 const error = () => {
@@ -8,6 +8,26 @@ const error = () => {
 const success = (position) => {
 	Geo.lat = position.coords.latitude;
 	Geo.lng = position.coords.longitude;
+	const Weather = "http://api.wunderground.com/api/" + key + "/forecast/geolookup/conditions/q/" + Geo.lat + "," + Geo.lng + ".json";
+	$.ajax({
+		url : Weather,
+		dataType : "jsonp",
+		success : (data) => {
+			console.log(data)
+			const location = data['location']['city'];
+			const temp = data['current_observation']['temp_f'];
+			const img = data['current_observation']['icon_url'];
+			const desc = data['current_observation']['weather'];
+			const wind = data['current_observation']['wind_string'];
+			//setting the spans to the correct parameters
+			$('#location').html(location);
+			$('#temp').html(temp);
+			$('#desc').html(desc);
+			$('#wind').html(wind);
+			//filling the image src attribute with the image url
+			$('#img').attr('src', img);
+		}
+	});
 }
 
 if (navigator.geolocation) {
@@ -18,12 +38,3 @@ else {
 }
 
 const key = "840e73603810f91a";
-const Weather = "http://api.wunderground.com/api/" + key + "/forecast/geolookup/conditions/q/" + Geo.lat + "," + Geo.lng + ".json";
-
-$.ajax({
-	url : Weather,
-	dataType : "json",
-	success : (data) => {
-		console.log(Weather)
-	}
-});
